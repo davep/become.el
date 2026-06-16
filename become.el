6525bced4affd2c98c49d45b8677592b580c6899
Dave Pearson <davep@davep.org>
2026-06-16 12:00:26 +0100

    :books: Remove full stop from the end of the summary

--- become.el
+++ become.el
@@ -1,4 +1,4 @@
-;;; become.el --- Tools for transforming a buffer.
+;;; become.el --- Tools for transforming a buffer
 ;; Copyright 2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
