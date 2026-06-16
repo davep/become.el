c47fa73373dfc8ffde019b9ce0349744dc8d64e2
Dave Pearson <davep@davep.org>
2026-06-16 12:01:51 +0100

    :card_index: Move to lexical binding

--- become.el
+++ become.el
@@ -1,4 +1,4 @@
-;;; become.el --- Tools for transforming a buffer
+;;; become.el --- Tools for transforming a buffer  -*- lexical-binding: t; -*-
 ;; Copyright 2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
